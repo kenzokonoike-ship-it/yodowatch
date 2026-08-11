b2fabdd5ebfda8247ae92f0d70524853478151e2
kenzokonoike-ship-it <kenzo.konoike@gmail.com>
2026-08-11 14:49:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+import express from "express";
+import fetch from "node-fetch";
 
+const app = express();
+
+app.get("/", async (req, res) => {
+  const url = "https://www.yodobashi.com/product/100000001009809342/";
+
+  const response = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        + "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+      "Accept":
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,"
+        + "image/webp,image/apng,*/*;q=0.8",
+      "Accept-Language": "ja,en-US;q=0.9,en;q=0.8"
+    }
+  });
+
+  const text = await response.text();
+  res.send(text);
+});
+
+app.listen(3000);
